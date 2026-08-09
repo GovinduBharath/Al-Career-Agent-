ab2de043f1768f97c0ec6101d38b06c1054d79a4
Bharath <24r21a66f1@mlrit.ac.in>
2026-08-09 19:02:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ if not GOOGLE_API_KEY:
 # ============================================================
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+    model="GOOGLE API KEY",
     google_api_key=GOOGLE_API_KEY,
     temperature=0
 )
